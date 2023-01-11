9b14de27c0545b6d3daf439d5873f4abe7486d89
Ramee00 <116334461+Ramee00@users.noreply.github.com>
2023-01-11 09:51:34 +0530

    Update dp_a.py

--- dp_a.py
+++ dp_a.py
@@ -1,5 +1,5 @@
-m_score=2
-mm_score=-1
+m_score=5
+mm_score=-4
 
 def match(i,j,arr):
     return( (arr[i-1][j-1])+m_score)
@@ -9,14 +9,14 @@ def mismatch(i,j,arr):
     
 
 def DP():
-    rows, cols = (3, 3)
+    rows, cols = (17, 17)
     arr = [[0 for i in range(cols)] for j in range(rows)]
     """for i in range(0,cols+1):
         arr[i][0]=0;
     for j in range(0,rows+1):
             arr[0][j]=0;"""
-    s1="AC"
-    s2="AG"
+    s1="AGCTAGCTCATGAGTC"
+    s2="GATCAGCACGAGTTCT"
     
     for i in range(1,rows):
         for j in range(1,cols):
